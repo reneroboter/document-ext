3e5c71b5b17dc8a08fa04b986cddc6cdb45ba469
René Backhaus <rene.backhaus@hdnet.de>
2017-04-11 16:27:53 +0200

    add basic stuff for calendar

---
+++ docroot/typo3conf/ext/documentext/Classes/Controller/CalendarController.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * CalendarController
+ */
+namespace ReneRoboter\Documentext\Controller;
+
+use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+
+/**
+ * CalendarController
+ */
+class CalendarController extends ActionController
+{
+    /**
+     * @plugin Calendar
+     */
+    public function calendarAction()
+    {
+
+    }
+}
