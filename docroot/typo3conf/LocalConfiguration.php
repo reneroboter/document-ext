28a864e14c24d7532705705bddcb80896d29f51a
René Backhaus <rene.backhaus@hdnet.de>
2017-04-12 10:27:35 +0200

    Update LocalConfiguration

--- docroot/typo3conf/LocalConfiguration.php
+++ docroot/typo3conf/LocalConfiguration.php
@@ -22,6 +22,11 @@ return [
             'saltedpasswords' => 'a:2:{s:3:"BE.";a:4:{s:21:"saltedPWHashingMethod";s:41:"TYPO3\\CMS\\Saltedpasswords\\Salt\\PhpassSalt";s:11:"forceSalted";i:0;s:15:"onlyAuthService";i:0;s:12:"updatePasswd";i:1;}s:3:"FE.";a:5:{s:7:"enabled";i:1;s:21:"saltedPWHashingMethod";s:41:"TYPO3\\CMS\\Saltedpasswords\\Salt\\PhpassSalt";s:11:"forceSalted";i:0;s:15:"onlyAuthService";i:0;s:12:"updatePasswd";i:1;}}',
         ],
     ],
+    'EXTCONF' => [
+        'lang' => [
+            'availableLanguages' => [],
+        ],
+    ],
     'FE' => [
         'debug' => false,
         'loginSecurityLevel' => 'rsa',
