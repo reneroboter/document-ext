8348bae1c490032300885fc4213e64f99391cfb7
René Backhaus <rene.backhaus@hdnet.de>
2017-04-12 10:25:13 +0200

    Set autoloader as extension dependency

--- docroot/typo3conf/ext/documentext/ext_emconf.php
+++ docroot/typo3conf/ext/documentext/ext_emconf.php
@@ -15,6 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '7.6.0-7.6.99',
+            'autoloader' => '1.0.0-5.5.99',
 		),
 		'conflicts' => array(
 		),
