d0c2c50ca98d5db4905e3de4dcda8755774bb13c
René Backhaus <rene.backhaus@hdnet.de>
2017-04-12 10:27:07 +0200

    Replace localconf and tables configuration with autoloader

--- docroot/typo3conf/ext/documentext/ext_localconf.php
+++ docroot/typo3conf/ext/documentext/ext_localconf.php
@@ -3,14 +3,9 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'ReneRoboter.' . $_EXTKEY,
-	'Documentext',
-	array(
-		'Content' => 'list,addForm,add,updateForm,update,deleteConfirm,delete,show,print',
-	),
-	// non-cacheable actions
-	array(
-		'Content' => 'list,addForm,add,updateForm,update,deleteConfirm,delete,show,print',
-	)
+\HDNET\Autoloader\Loader::extLocalconf(
+    'ReneRoboter',
+    'documentext',
+    \ReneRoboter\Documentext\Utility\RegisterUtility::getAutoloaderLoader()
 );
+
--- docroot/typo3conf/ext/documentext/ext_tables.php
+++ docroot/typo3conf/ext/documentext/ext_tables.php
@@ -3,13 +3,8 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	'ReneRoboter.' . $_EXTKEY,
-	'Documentext',
-	'documentExt - Ausbildungsdokumentation'
+\HDNET\Autoloader\Loader::extTables(
+    'ReneRoboter',
+    'documentext',
+    \ReneRoboter\DocumentExt\Utility\RegisterUtility::getAutoloaderLoader()
 );
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'documentExt');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_documentext_domain_model_content', 'EXT:documentext/Resources/Private/Language/locallang_csh_tx_documentext_domain_model_content.xlf');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_documentext_domain_model_content');
